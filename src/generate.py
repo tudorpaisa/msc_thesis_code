be58719643458ff556f2d046db49f8d513ec7dd2
tudorpaisa <tudor.paisa@gmail.com>
2019-04-30 09:55:05 +0200

    We generate better now

--- src/generate.py
+++ src/generate.py
@@ -19,11 +19,14 @@ if __name__ == '__main__':
     median_len = int(df['seq_len'].median())
 
     greed = False
-    temperature = 1.0
+    temperatures = [0.8, 0.9, 1.0, 1.1, 1.2]
 
     models = ['performance_rnn']
 
+    make_folder('../generated')
     for model in models:
+        make_folder(f'../generated/{model}')
+
         checkpoint = load_model_data(os.path.join('../models/', model))
         if model == 'baseline':
             from lstm_baseline import Baseline
@@ -51,19 +54,33 @@ if __name__ == '__main__':
                 use_bias=checkpoint['use_bias'],
                 is_bidirectional=checkpoint['is_bidirectional'])
             net.load_state_dict(checkpoint['model_state'])
-        for i in tqdm(range(1, 2), desc=f'Generating from {model}'):
-            init = torch.randn(net.batch_size, net.init_dim).to(device)
-            steps = int(median_len * (RNG.randint(90, 111) / 100))
-            song = net.generate(
-                init,
-                steps,
-                greedy=greed,
-                temperature=temperature,
-                output_type='index')
-            song = song.squeeze().tolist()
-            make_folder('../generated')
-            song = from_seq(song)
-            song.save(
-                filename=os.path.join(
-                    '../generated/',
-                    f'{model}_{i}_{greed}_{int(temperature*10)}.midi'))
+        elif model == 'c_rnn_gan':
+            from c_rnn_gan import Generator
+            net = Generator(
+                in_dim=checkpoint['in_dim'],
+                hidden_dim=checkpoint['hidden_dim'],
+                batch_size=1,  # we create one song at a time
+                out_dim=checkpoint['out_dim'],
+                dropout=checkpoint['dropout'],
+                num_layers=checkpoint['num_layers'],
+                init_dim=checkpoint['init_dim'],
+                use_bias=checkpoint['use_bias'])
+
+        for temp in temperatures:
+            make_folder(f'../generated/{model}/{temp}')
+
+            for i in tqdm(
+                    range(1, 126),
+                    desc=f'Generating from {model} with {temp}'):
+                init = torch.randn(net.batch_size, net.init_dim).to(device)
+                steps = int(median_len * (RNG.randint(90, 111) / 100))
+                song = net.generate(
+                    init,
+                    steps,
+                    greedy=greed,
+                    temperature=temp,
+                    output_type='index')
+                song = song.squeeze().tolist()
+                song = from_seq(song)
+                song.save(
+                    filename='../generated/{model}/{int(temp*10)}/{i}.midi')
